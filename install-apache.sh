98dd47039c5c7a1bbc80dd29bcd27c12d210bc52
Jules Chiron <chironjules.78@gmail.com>
2024-11-04 16:52:23 -0500

    Copy html file from repo

--- install-apache.sh
+++ install-apache.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# Met à jour les paquets
+# Met a jour les paquets
 sudo apt-get update
 # Installe Apache2
 sudo apt-get install -y apache2
-# Démarre Apache2 et l'active au démarrage
+# Demarre Apache2 et l'active au demarrage
 sudo systemctl start apache2
 sudo systemctl enable apache2
+
+mv /var/www/html/index.html /var/www/html/index.html.old
+wget https://raw.githubusercontent.com/Boucanier/test-azure/main/test.html -O /var/www/html/index.html
