48b89444c9b4383d59978f03705d1441bffea64c
Jules Chiron <chironjules.78@gmail.com>
2024-11-08 21:22:55 -0500

    Change port

--- website/app.mjs
+++ website/app.mjs
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Configurer le moteur de template EJS
 app.set('view engine', 'ejs');
