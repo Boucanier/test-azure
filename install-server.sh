6cee3c76a964637c3c9fbde7ad7518f0d21a8960
Jules Chiron <chironjules.78@gmail.com>
2024-11-08 21:15:58 -0500

    Complete installation

--- install-server.sh
+++ install-server.sh
@@ -6,7 +6,10 @@ sudo apt-get install -y nodejs npm
 
 mkdir /home/azureadmin/website
 
-wget https://raw.githubusercontent.com/Boucanier/test-azure/main/website/app.js -O /home/azureadmin/website/app.js
+wget https://raw.githubusercontent.com/Boucanier/test-azure/main/website/app.mjs -O /home/azureadmin/website/app.mjs
+wget https://raw.githubusercontent.com/Boucanier/test-azure/main/website/public/images/UQAC_logo.png -O /home/azureadmin/website/public/images/UQAC_logo.png
+wget https://raw.githubusercontent.com/Boucanier/test-azure/main/website/public/styles/style.css -O /home/azureadmin/website/public/styles/style.css
+wget https://raw.githubusercontent.com/Boucanier/test-azure/main/website/views/index.ejs -O /home/azureadmin/website/views/index.ejs
 wget https://raw.githubusercontent.com/Boucanier/test-azure/main/website/package.json -O /home/azureadmin/website/package.json
 
 cd /home/azureadmin/website
